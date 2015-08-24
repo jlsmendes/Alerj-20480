d337ba6510c6b0682a1fcaf6e9504c4e0d6f141b
jlsmendes <jlsmendes@gmail.com>
2015-08-24 16:52:14 -0700

    texto

---
+++ Allen20480/Allen20480/Scripts/Apps/ajaxUtil.js
@@ -0,0 +1,19 @@
+﻿var Ajax = function (options) {
+    var _url = options.url;
+    var _type = options.type;
+    var _dataType = options.dataType;
+    var _contentType = options.contentType;
+    var _data = options.data;
+
+    this.makeRequest = function (beforeSend) {
+        return $.ajax({
+            url: _url,
+            type: _type,
+            dataType: _dataType,
+            contentType: _contentType,
+            data: _data,
+            beforeSend: beforeSend
+        }).promise();
+    };
+};
+
